0be312577bd56b09ffe11a376cfd139604991238
Ravisk041 <106578215+Ravisk041@users.noreply.github.com>
2025-12-17 16:25:55 +0530

    Update simple mobile price finding.py

--- simple mobile price finding.py
+++ simple mobile price finding.py
@@ -1,11 +1,18 @@
-t1=input('enter model:')
-t2=int(input('enter purchase year'))
-t3=int(input('enter price'))
+t1 = input('enter model: ')
+t2 = int(input('enter purchase year: '))
+t3 = int(input('enter price: '))
+
 if t1 == 'iphone' or t1 == 'google' or t1 == 'samsungs24':
-  if t2 == 2024:
-    price = t3
-    print(f'{t1} is costing {price} which bought in the year {t2}')
-  elif t2 == 2025:
-    price1 = price * 0.20
-    print(f'{t1} is costing {price+price1} due to the increase in tax rate in the year {t2}')
-    
+    price = t3   # assign once
+
+    if t2 == 2024:
+        print(f'{t1} is costing {price} which was bought in the year {t2}')
+
+    elif t2 == 2025:
+        new_price = price * 1.20
+        print(f'{t1} is costing {new_price} due to the increase in tax rate in the year {t2}')
+
+    else:
+        print('Invalid year')
+else:
+    print('Invalid model')
